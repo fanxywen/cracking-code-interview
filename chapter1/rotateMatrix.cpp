79b75778ed82a1219fcbaf8da1b7884b3c46da48
wg72 <wg72@duke.edu>
2019-03-26 20:23:14 -0400

    chapter1 complete

--- chapter1/rotateMatrix.cpp
+++ chapter1/rotateMatrix.cpp
@@ -15,6 +15,22 @@ void rotate(vector<vector<int> >& matrix){
   }
 
   for(int i = 0; i < n; ++i){
-    
+    for(int j = i+1; j < n; ++j){
+      int temp = matrix[i][j];
+      matrix[i][j] = matrix[j][i];
+      matrix[j][i] = temp;
+    }    
   }
+}
+
+int main(){
+  vector<vector<int> > matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+  rotate(matrix);
+  for(int i = 0; i < matrix.size(); i++){
+    for(int j = 0; j < matrix.size(); ++j){
+      cout<<matrix[i][j]<<" ";
+    }
+    cout<<endl;
+  }
+  return 0;
 }
